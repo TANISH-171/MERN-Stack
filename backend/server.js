25d8b59c911342480ac10a710c867b23748814bf
TANISH-171 <tanish9495@gmail.com>
2025-09-22 20:10:05 +0530

    On the way

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
 
 const app=express();
+
+app.get('/api/notes',(req,res)=>{
+    res.status(201).send("Hello from backend and front");
+});
+
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
